73347c809df8238ae6fe0a581327b6b2ead2d710
jrk9451 <70785755+jrk9451@users.noreply.github.com>
2023-07-02 14:55:37 +0530

    Add files via upload

---
+++ Declare variable.java
@@ -0,0 +1,8 @@
+class Guru99 {
+    static int a = 1; //static variable  
+    int data = 99; //instance variable  
+    void method() {
+        int b = 90; //local variable  
+        string str= jay;//local variable
+    }
+}
